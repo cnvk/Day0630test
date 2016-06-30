44786e0621ec5e125db3cb33c5d0f3e39a4f8d05
cnvk <cnvk582@163.com>
2016-06-30 16:11:41 +0800

    Rejected commit: Default

--- app/src/main/java/my/microsoft/com/day0630test/MainActivity.java
+++ app/src/main/java/my/microsoft/com/day0630test/MainActivity.java
@@ -2,8 +2,11 @@ package my.microsoft.com.day0630test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tv;
+    TextView tv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
