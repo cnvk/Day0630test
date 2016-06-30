572e07e32cfd08c6105301ffcbe29e14252f0e31
cnvk <cnvk582@163.com>
2016-06-30 15:53:53 +0800

    Rejected commit: Default

---
+++ app/src/main/java/my/microsoft/com/day0630test/MainActivity.java
@@ -0,0 +1,13 @@
+package my.microsoft.com.day0630test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
